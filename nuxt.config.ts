aebcb1c4fd0d9edc4814c6df2606184da1ad84df
alexlyaps <a34270622@gmail.com>
2025-08-01 18:44:18 +0200

    fix/theme-color

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -27,6 +27,7 @@ export default defineNuxtConfig({
         { name: "viewport", content: "width=device-width, initial-scale=1" },
         { name: "description", content: "Alma" },
         { name: "apple-mobile-web-app-title", content: "Alma" },
+        { name: "theme-color", content: "#ffffff" },
       ],
       link: [
         { rel: "icon", type: "image/png", href: "/my-favicon.ico" },
@@ -36,13 +37,14 @@ export default defineNuxtConfig({
           href: "/favicon-96x96.png",
           sizes: "96x96",
         },
+
         { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
         { rel: "shortcut icon", href: "/favicon.ico" },
-        // {
-        //   rel: "apple-touch-icon",
-        //   sizes: "180x180",
-        //   href: "/apple-touch-icon.png",
-        // },
+        {
+          rel: "apple-touch-icon",
+          sizes: "180x180",
+          href: "/apple-touch-icon.png",
+        },
         { rel: "manifest", href: "/site.webmanifest" },
       ],
     },
